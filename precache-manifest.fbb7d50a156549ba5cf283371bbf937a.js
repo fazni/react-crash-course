c37efdef72eb5e8d185365b09793783213ab04c1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-21 20:03:23 +0000

    Deploy fazni/react-crash-course to github.com/fazni/react-crash-course.git:gh-pages

---
+++ precache-manifest.fbb7d50a156549ba5cf283371bbf937a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef191a01ef8e269f0ca1440c0c233af",
+    "url": "/react-crash-course/index.html"
+  },
+  {
+    "revision": "64e09f864673e3e8235a",
+    "url": "/react-crash-course/static/css/main.ecc5edc1.chunk.css"
+  },
+  {
+    "revision": "19f20d49257290b12771",
+    "url": "/react-crash-course/static/js/2.37cfde04.chunk.js"
+  },
+  {
+    "revision": "64e09f864673e3e8235a",
+    "url": "/react-crash-course/static/js/main.289a2413.chunk.js"
+  },
+  {
+    "revision": "6e4762fda08096d33823",
+    "url": "/react-crash-course/static/js/runtime-main.268b7f73.js"
+  }
+]);
